68021015e38e80e70f9d7d490ff10403deb3104b
Andrew Zelenov <andrew.zelenov@aol.com>
2019-08-15 13:31:41 +0200

    fixing bugs on new master updated branch

--- macro/ShipReco.py
+++ macro/ShipReco.py
@@ -184,8 +184,8 @@ for iEvent in range(firstEvent, nEvents):
  # memory monitoring
  # mem_monitor() 
 # end loop over events
-h['vshape_original'] = ROOT.strawtubesDigi.Instance().initialVShape.Clone()
-h['residuals'] = ROOT.strawtubesDigi.Instance().residualsInStraw.Clone()
+# h['vshape_original'] = ROOT.strawtubesDigi.Instance().initialVShape.Clone()
+# h['residuals'] = ROOT.strawtubesDigi.Instance().residualsInStraw.Clone()
 print ROOT.strawtubesDigi.Instance().counter
 # ut.bookCanvas(h,key='dist',title='dist',nx=1200,ny=600,cx=3,cy=1)
 # cv=h['dist'].cd(1)
