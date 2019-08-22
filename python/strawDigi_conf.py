91ef60629acddc3c9d227c99e857ea7c23d8ff91
Andrew Zelenov <andrew.zelenov@aol.com>
2019-08-22 16:16:32 +0200

    only for comparison

--- python/strawDigi_conf.py
+++ python/strawDigi_conf.py
@@ -2,7 +2,7 @@ import shipunit as u
 
 class StrawtubesMisalign:
     # TurnOn the module of misalign or no
-    misalign = True
+    misalign = False
     # rand type : "None", "Gaus", "Unif", may added more, need modify strawtubeDigi
     # "None" = all tubes have same maximum sagging, no distribution of max. sag
     # "Gaus" = the tubes have different max sagging with gaus distribution
@@ -12,7 +12,7 @@ class StrawtubesMisalign:
     # maximum sagging at the middle of the tube
     # for using distribution, these are mean value /mpv /something like that
     maxTubeSagging = 0.0*u.cm
-    maxWireSagging = 0.7*u.cm
+    maxWireSagging = 0.0*u.cm
 
     # uniform distribtion, the delta is half of the range
     tubeUnifDelta = 0.0*u.cm
@@ -27,6 +27,6 @@ class StrawtubesMisalign:
 
 # Using which way to calculate drift time
 class DriftTimeCalculate:
-    defaultDriftTime = True   # true for using drift velocity (default)
+    defaultDriftTime = False   # true for using drift velocity (default)
 
 
